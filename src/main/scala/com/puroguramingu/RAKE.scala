7302a20f17faa034c663234c220b911f42ce9943
mdymczyk <dymczyk@gmail.com>
2016-09-20 00:06:17 +0900

    Improve current documentation

--- src/main/scala/com/puroguramingu/RAKE.scala
+++ src/main/scala/com/puroguramingu/RAKE.scala
@@ -26,18 +26,39 @@ final class RAKE(val stopwords: Set[String],
                  override val uid: String) extends Estimator[RAKEModel] {
 
   def this(stopwords: Set[String],
-           wordDelims: Array[Char],
-           phraseDelims: Array[Char]) =
+           wordDelims: Array[Char] = Array[Char](' ', '\t'),
+           phraseDelims: Array[Char] = Array[Char](',', '.')) =
     this(stopwords, wordDelims, phraseDelims, Identifiable.randomUID("rake"))
 
   override def fit(dataset: Dataset[_]): RAKEModel = ???
+
+  def fit(dataset: RDD[String]): RAKEModel = {
+    dataset.foreach(toRAKESeq(_))
+    null
+  }
+
+  override def copy(extra: ParamMap): Estimator[RAKEModel] = ???
+
+  override def transformSchema(schema: StructType): StructType = ???
 
   /**
     * Transforms an input document into a RAKE sequence of keywords,
     * where each keyword is a sequence of one or more tokens.
     *
-    * This method run in linear time as a state machine checking
+    * This method runs in linear time (proportional to the number of
+    * characters in the document) as a state machine checking
     * the whole document character by character.
+    *
+    * Example:
+    *
+    * Document: I ate cake, pie and a blueberry scone and then strawberry pie.
+    * Stopwords: ["I", "a", "and", "then"]
+    * Word delimeters: [' ']
+    * Phrase delimeters: ['.', ',']
+    *
+    * Would result in a following sequence of sequences:
+    *
+    * [ ["ate", "cake"], ["pie"], ["blueberry", "scone"], ["strawberry", "pie"] ]
     *
     * @param doc The document to be sequencialized
     * @return
@@ -81,6 +102,31 @@ final class RAKE(val stopwords: Set[String],
     * the frequency of each word (how many times it appeared in the whole corpus)
     * and the degree of each word (what is the total size of all the phrases in which
     * a given word occurred).
+    * Example:
+    *
+    * Document: I ate cake, pie and a blueberry scone and then strawberry pie.
+    * Stopwords: ["I", "a", "and", "then"]
+    * Word delimeters: [' ']
+    * Phrase delimeters: ['.', ',']
+    *
+    * Would result in the following 3 structures:
+    * [ ["ate", "cake"], ["pie"], ["blueberry", "scone"], ["strawberry", "pie"] ]
+    *
+    * Cooccurrence matrix:
+    * [
+    *   "ate" -> ["ate" -> 1, "cake" -> 1],
+    *   "cake" -> ["cake" -> 1, "ate" -> 1],
+    *   "pie" -> ["pie" -> 2, "strawberry" -> 1],
+    *   "blueberry" -> ["blueberry" -> 1, "scone" -> 1],
+    *   "scone" -> ["scone" -> 1, "blueberry" -> 1],
+    *   "strawberry" -> ["strawberry" -> 1, "pie" -> 1]
+    * ]
+    *
+    * Frequency:
+    * ["ate" -> 1, "cake" -> 1, "pie" -> 2, "blueberry" -> 1, "scone" -> 1, "strawberry" -> 1]
+    *
+    * Degree:
+    * ["ate" -> 2, "cake" -> 2, "pie" -> 3, "blueberry" -> 2, "scone" -> 2, "strawberry" -> 1]
     *
     * @param corpus The corpus to be examined, which consists of a sequence of phrases (sequences
     *               of [[String]])
@@ -91,7 +137,7 @@ final class RAKE(val stopwords: Set[String],
     val coocMat = MMap[String, MMap[String, Long]]()
     val deg = MMap[String, Long]()
     val freq = MMap[String, Long]()
-    // TODO threadsafety?!
+    // TODO threadsafety?
     corpus.foreach { doc =>
       for (i <- doc.indices) {
         freq.put(doc(i), freq.getOrElse[Long](doc(i), 0) + 1)
@@ -111,14 +157,6 @@ final class RAKE(val stopwords: Set[String],
     (coocMat, deg, freq)
   }
 
-  def fit(dataset: RDD[String]): RAKEModel = {
-    dataset.foreach(toRAKESeq(_))
-    null
-  }
-
-  override def copy(extra: ParamMap): Estimator[RAKEModel] = ???
-
-  override def transformSchema(schema: StructType): StructType = ???
 }
 
 class RAKEModel(override val uid: String) extends Model[RAKEModel] {
